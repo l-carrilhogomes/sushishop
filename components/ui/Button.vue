a3c1bbc7a27e6902ae0de6471ea119ed237fed12
Lucas <carrilhogomeslucas@gmail.com>
2025-05-22 00:44:20 +0200

    hover button color

--- components/ui/Button.vue
+++ components/ui/Button.vue
@@ -14,7 +14,7 @@ defineProps({
 
 <template>
   <button
-    class="bg-yellow-400 text-[#0a0a0a] font-bold px-6 py-2 rounded-lg transition-colors duration-300"
+    class="bg-yellow-400 text-[#0a0a0a] font-bold px-6 py-2 rounded-lg transition-colors duration-300 hover:bg-yellow-500"
     @click="onClick"
   >
     {{ label }}
